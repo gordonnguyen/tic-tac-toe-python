becf5968677b1d29425864369720f5d255613ff1
Gordon Nguyen <maixanh45@yahoo.com>
2021-02-25 09:42:29 -0600

    Changed directories for tictactoe

--- ProductPriceComparison/PriceComparisonMain.py
+++ ProductPriceComparison/PriceComparisonMain.py
@@ -6,13 +6,25 @@ import openpyxl
 def main():
     watchlist_workbook = openpyxl.load_workbook('ProductWatchList.xlsx')
     sheet = watchlist_workbook.get_sheet_by_name('Sheet1')
+    price_list = []
+    name_list = []
+    price_list, name_list = getNameandPrice(sheet)
+
+
+    # Convert this:
+    #save_price_and_name(product_price, product_name, sheet, watchlist_workbook)
+
+def getNameandPrice(sheet):
+    price_list = []
+    name_list = []
 
     # Convert to list (multiple products)
-    for row in range()
-    price_list, product_name = getProductPrice(sheet.cell(row,col).value)
-    #product_price, product_name = getProductPrice(sheet['F2'].value)
+    for i in sheet:
+        price_list, name_list = getProductPrice(sheet.cell(row,col).value)
+        #product_price, product_name = getProductPrice(sheet['F2'].value)
+        
     print('The product_price of \"'+product_name+'\" on Ebay is',product_price)
-    save_price_and_name(product_price, product_name, sheet, watchlist_workbook)
+
 
 def save_price_and_name(product_price, product_name, sheet, watchlist_workbook):
     sheet['A2'].value = product_name
